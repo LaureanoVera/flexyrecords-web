13db53182847099fd71e71ecd639a808d3dba12b
LaureanoVera <laureanoigvera@gmail.com>
2021-07-12 09:00:59 -0300

    middlewares, morgan body-parser

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+const bodyParser = require("body-parser");
 const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3006;
+
+// MIDDLEWARE
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true })); // middleware para analizar cuerpos desde URL
+app.use(bodyParser.json({ extended: true })); //middleware para analizar objetos json
 
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
+app.set("views", path.join(__dirname, "views"));
 
 // STATICS
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // ROUTES
-app.use('/', require('./routes/Index'))
+app.use("/", require("./routes/Index"));
 
 // 404 ERROR
 app.use((req, res, next) => {
